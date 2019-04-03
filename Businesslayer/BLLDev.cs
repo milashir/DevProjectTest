b0c40c4227a5dfd5c578567b1426bd6556ce8973
milashir <48650884+milashir@users.noreply.github.com>
2019-04-03 14:11:29 +0200

    Add files via upload

---
+++ Businesslayer/BLLDev.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer;
+using EnitityLayer;
+
+namespace Businesslayer
+{
+    public class BLLDev
+    {
+        DLLDev objDLLDev = new DLLDev();
+
+        /// <summary>
+        /// Get all products
+        /// </summary>
+        /// <returns></returns>
+        public List<Product> GetAllProducts()
+        {
+            List<Product> lstProduct = new List<Product>();
+            return objDLLDev.GetAllProducts();
+        }
+
+        /// <summary>
+        /// Get product by Id and Quantity
+        /// </summary>
+        /// <returns>List<Product></returns>
+        public List<Product> GetProductByIdandQuantity(int productid, int quantity)
+        {
+            List<Product> lstProduct = objDLLDev.GetProductByIdandQuantity(productid, quantity);
+            return lstProduct;
+        }
+    }
+}
